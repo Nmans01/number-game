996dd8c1a36fafdc1f038f34974f3fe0fcf7ddbe
Nate <natemans01@gmail.com>
2022-10-22 23:48:11 -0400

    Added basic UI and functionality loop

--- js/script.js
+++ js/script.js
@@ -1,9 +1,72 @@
-() => {
+(function () {
+    "use strict";
 
-document.addEventListener('load',init);
+    let queue;
+    let stack;
+    let stupidThing;
+    let shapes;
+    let round;
 
-function init () {
-    
-}
+    window.addEventListener('load',init);
 
-}
+    function init () {
+        queue = document.getElementById('queue').children;
+        stack = document.getElementById('stack');
+        stupidThing = document.getElementById('stack').cloneNode(true);
+        shapes = stupidThing.children;
+
+        round = 0;
+
+        stack.innerHTML = '';
+
+        window.addEventListener('keydown',onKeydown);
+    }
+
+    function onKeydown (e) {
+        e.preventDefault();
+        // console.log(e.key);
+        switch (e.key) {
+            case 'ArrowUp':
+                break;
+            case 'ArrowDown':
+                break;
+            case ' ':
+                drop();
+                break;
+            case 'ArrowRight':
+                break;
+        }
+    }
+
+    function drop() {
+        let shape=queue[0].children[0];
+        if (shape==undefined)
+            return;
+        // console.log(shape);
+        stack.appendChild(queue[0].removeChild(shape));
+        shape = shape=queue[1].children[0];
+        if (shape==undefined)
+            return;
+        queue[0].appendChild(shape);
+        queue[1].appendChild(getNew());
+        update();
+    }
+
+    function getNew () {
+        let x = Math.random();
+        let i = Math.floor( curve(sigmoid(round/10)*x) *shapes.length);
+        return(shapes[i].cloneNode(true));
+    }
+    function sigmoid(z) {
+        return 1 / (1 + Math.exp(-z));
+    }
+    function curve(z) {
+        return (Math.pow(z,4)+Math.pow(z,3)+z)/3;
+    }
+
+    function update () {
+        
+        round+=1;
+        console.log("round: " + round);
+    }
+})();
