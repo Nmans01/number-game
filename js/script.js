c5f56000b049932e5dd0c0eb6fd3459bd9e7739e
Nate <natemans01@gmail.com>
2022-10-25 00:37:51 -0400

    Added multiple game functions

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@
     let stupidThing;
     let shapes;
     let round;
+    let connectIndicator;
+    let connectWrapper;
 
     window.addEventListener('load',init);
 
@@ -17,9 +19,16 @@
 
         round = 0;
 
+        connectIndicator = document.getElementById('connectIndicator')
+        connectIndicator.remove();
+        connectWrapper = {'shapes':[],'sum':0}; // "its just a list"
+
+
         stack.innerHTML = '';
 
         window.addEventListener('keydown',onKeydown);
+        window.addEventListener('mousedown',onMousedown);
+        window.addEventListener('contextmenu',e => e.preventDefault()); //disable rightclick menu
     }
 
     function onKeydown (e) {
@@ -27,8 +36,12 @@
         // console.log(e.key);
         switch (e.key) {
             case 'ArrowUp':
+                bottomToTop();
+                drop();
                 break;
             case 'ArrowDown':
+                topToBottom();
+                drop();
                 break;
             case ' ':
                 drop();
@@ -36,6 +49,65 @@
             case 'ArrowRight':
                 break;
         }
+    }
+    function onMousedown (e) {
+        e.preventDefault();
+        let shape = shapeTargeted(e.target);
+        if (shape && e.button == 0 && connectWrapper.sum == 0) {
+            
+            let stackArray = Array.from(stack.children);
+            let startIndex = stackArray.indexOf(shape);
+            let endIndex;
+            let maxIndex;
+            let minIndex;
+            let sum;
+
+            let connect = function (e) {
+                let shape = shapeTargeted(e.target);
+                if (!shape) return;
+
+                endIndex = stackArray.indexOf(shape);
+
+                maxIndex = Math.max(startIndex,endIndex);
+                minIndex = Math.min(startIndex,endIndex);
+
+                sum = 0;
+                for (let i=0; i<stack.children.length; i++) {
+                    if (minIndex <= i && i <= maxIndex) {
+                        stackArray[i].classList.add('connected');
+                        sum += parseInt(stackArray[i].children[0].innerText);
+                    } else {
+                        stackArray[i].classList.remove('connected');
+                    }
+                }
+                
+                stackArray[minIndex].appendChild(connectIndicator);
+                connectIndicator.innerText=sum;
+            };
+
+            window.addEventListener('mouseover', connect);
+
+            window.addEventListener('mouseup', (e) => {
+                window.removeEventListener('mouseover', connect);
+                console.log(minIndex+" "+maxIndex);
+                if (minIndex!=maxIndex) {
+                    connectWrapper.shapes = stackArray.slice(minIndex,maxIndex+1);
+                    connectWrapper.sum = sum;
+                    update();
+                } else {
+                    stackArray.forEach(a => a.classList.remove('connected'));
+                    connectIndicator.remove();
+                }
+            });
+        }
+    }
+    function shapeTargeted (t) {
+        while (t.parentNode!=stack) {
+            if (t.parentNode==document)
+                return null;
+            t=t.parentNode;
+        }
+        return t;
     }
 
     function drop() {
@@ -46,6 +118,16 @@
         queue[0].appendChild(shape);
         queue[1].appendChild(getNew());
         update();
+        round++;
+        console.log("round: " + round);
+    }
+    function topToBottom() {
+        let top = stack.children[stack.children.length-1];
+        stack.insertBefore(top,stack.children[0]);
+    }
+    function bottomToTop() {
+        let bottom = stack.children[0];
+        stack.appendChild(bottom);
     }
 
     function getNew () {
@@ -61,18 +143,38 @@
     }
 
     function update () {
-        for (let i=0; i<stack.children.length-1; i++) {
-            // console.log("doot");
+        let weirdified = [];
+        for (let i=0; i<stack.children.length; i++) {
             let a = stack.children[i];
-            let b = stack.children[i+1];
-            if (a.children[0].innerText == b.children[0].innerText) {
-                a.children[0].innerText = parseInt(a.children[0].innerText) + parseInt(b.children[0].innerText);
-                stack.removeChild(b);
-                i=0; // check through the whole stack again
+            if (a.classList.contains('connected')) {
+                weirdified.push(connectWrapper);
+                i+=connectWrapper.shapes.length-1;
+            } else {
+                weirdified.push({'shapes':[a],'sum':parseInt(a.children[0].innerText)});
             }
         }
 
-        round++;
-        console.log("round: " + round);
+        console.log(weirdified);
+        for (let i=0; i<weirdified.length-1; i++) {
+            let a = weirdified[i];
+            let b = weirdified[i+1];
+
+            // make b the connectWrapper if there is one
+            if (a.shapes.length>b.shapes.length) {
+                let c = a;
+                a = b;
+                b = c;
+            }
+
+            if (a.sum == b.sum) {
+
+                a.shapes[0].children[0].innerText = a.sum + b.sum;
+                b.shapes.forEach(shape => stack.removeChild(shape));
+                b.shapes = [];
+                b.sum = 0;
+                update(); // check through the whole stack again
+                return;
+            }
+        }
     }
 })();
